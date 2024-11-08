73675f5dd481299133263c184191ebe06fa63f0b
Caplitero <98284271+Caplitero@users.noreply.github.com>
2024-11-08 10:46:04 +0200

    Added image preview

--- script.js
+++ script.js
@@ -116,3 +116,17 @@ function processCSV(csvData, selectedColumns, selectedImageNameColumns, delimite
         });
     };
 }
+
+document.getElementById('image_file').addEventListener('change', function(event) {
+    const file = event.target.files[0];
+    const preview = document.getElementById('image_preview');
+    
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            // Display the image in the preview container
+            preview.innerHTML = `<img src="${e.target.result}" alt="Image Preview" style="width: 100%;">`;
+        };
+        reader.readAsDataURL(file);
+    }
+});
